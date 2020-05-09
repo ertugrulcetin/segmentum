0117202d707be10ef99b78432e5d2c172f30c7ac
Burkay Durdu <burkaydurdu@outlook.com>
2020-05-09 20:01:35 +0300

    create tranforms files and edit code base

--- src/segmentum/transformations/analytics.clj
+++ src/segmentum/transformations/analytics.clj
@@ -1,45 +1,26 @@
 (ns segmentum.transformations.analytics
   (:require [segmentum.transformations.google-analytics :refer [google-analytics-handler]]
             [camel-snake-kebab.core :as csk]
-            [camel-snake-kebab.extras :as cske]))
+            [camel-snake-kebab.extras :as cske]
+            [clojure.edn :as edn]
+            [clojure.java.io :as io]))
 
 
-(defn segmentum-event-params [data]
+(defn segmentum-event-params [valid-keys data]
   "This function should return segmentum event params"
-  (select-keys data [:type :category :action :label :value :client-id
-                     :anonymize-ip :data-source :user-id :session-control
-                     :user-agent :ip-override :geographical :cache-buster
-                     :document-referrer :campaign-name :campaign-source
-                     :campaign-medium :campaign-keyword :campaign-content
-                     :campaign-id :screen-resolution :viewport-size
-                     :document-encoding :screen-colors :user-language
-                     :java-enabled :flash-version :document-location-url
-                     :document-host-name :document-path :document-title
-                     :screen-name :content-group :link-id :application-name
-                     :application-id :application-version :application-installer-id
-                     :transaction-id :transaction-affiliation :transaction-revenue
-                     :transaction-shipping :transaction-tax :item-name :item-price
-                     :item-quantity :item-code :item-category :transaction-id
-                     :affiliation :revenue :tax :shipping :coupon-code
-                     :product-action-list :checkout-step :checkout-step-option
-                     :currency-code :social-network :social-action
-                     :social-action-target :user-timing-category :user-timing-time
-                     :user-timing-variable-name :user-timing-label :page-load-time
-                     :dns-time :page-download-time :redirect-response-time
-                     :tcp-connect-time :server-response-time :dom-interactive-time
-                     :content-load-time :exception-description :is-exception-fatal
-                     :disabling-advertising-personalization :queue-time]))
+  (select-keys data valid-keys))
 
 
-(defn segmentum-integration-routes [integration-key params]
+(defn segmentum-integration-routes [integration-key mappings params]
   (case integration-key
-    :google (google-analytics-handler params)))
+    :google (google-analytics-handler params mappings)))
 
 
 (defn segmentum-event-handler [params integration-key]
-  (->> params
-    (cske/transform-keys csk/->kebab-case)
-    segmentum-event-params
-    (segmentum-integration-routes integration-key)))
-
-
+  (let [transform-data (-> (io/resource "transforms/analytics/google.edn")
+                         slurp
+                         edn/read-string)]
+    (->> params
+      (cske/transform-keys csk/->kebab-case)
+      (segmentum-event-params (:valid-keys transform-data))
+      (segmentum-integration-routes integration-key (:mappings transform-data)))))
