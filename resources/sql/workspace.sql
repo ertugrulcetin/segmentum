f3a4b3e2850f84915c05e3a60ceb59569dbfd526
ertucetin <ertu.ctn@gmail.com>
2020-05-10 21:31:35 +0300

    Adding Workspace model

---
+++ resources/sql/workspace.sql
@@ -0,0 +1,15 @@
+-- :name create-workspace! :insert :raw
+-- :doc creates a new workspace record
+INSERT INTO workspace (created_by, name)
+VALUES (:created_by, :name);
+
+
+-- :name get-workspaces :? :*
+-- :doc retrieves all workspaces
+SELECT * FROM workspace;
+
+
+-- :name get-workspace :? :1
+-- :doc retrieves a workspace record with given id
+SELECT * FROM workspace AS w
+WHERE id = :id;
