5b43962cf7a682f068e54c766cda2df1ee54df55
ertucetin <ertu.ctn@gmail.com>
2020-05-19 21:18:57 +0300

    Fixing flow bug, replaced with d/batch

--- src/segmentum/api/event.clj
+++ src/segmentum/api/event.clj
@@ -18,97 +18,77 @@
 (defonce stream (s/stream 1024 xf))
 (defonce put! (throttle-fn (partial s/put! stream) 1000 :second))
 
-
 (def db-xf (map #(vector (:id %) (:write_key %) (:payload %) (:arrived_at %))))
 (defonce db-stream (s/stream 1024 db-xf))
 (defonce dest-stream (s/stream 1024))
 
-(def failed-write-xf (map #(assoc % :retry 0)))
-(defonce failed-write-stream (s/stream 512 failed-write-xf))
-
+(def fail-xf (map #(assoc % :retry 0)))
+(defonce failed-write-stream (s/stream 512 fail-xf))
 
 (s/connect stream db-stream)
 ;;TODO backpressure'a neden oluyor
 ;(s/connect stream dest-stream)
 
 
-(def h {"User-Agent" "Mozilla/5.0 (Windows NT 6.1;) Gecko/20100101 Firefox/13.0.1"})
-
-
-(defn- write-to-db? [event bulk-events]
-  (or (and (= ::timeout event)
-        (pos? (count bulk-events)))
-    (and (not= ::timeout event)
-      (= (count bulk-events) 32))))
-
-
 (defn- get-db-fn-by-type [type]
   (case type
-    :event (partial db/query :create-event!)))
+    :raw (partial db/query :create-event!)))
 
 
-(defn process-failed-db-writes []
-  (mc/async-loop 1 [events []]
-    (s/try-take! failed-write-stream ::drained 10000 ::timeout)
+(defn- try-write-to-db
+  ([event events]
+   (try-write-to-db event events true))
+  ([event events new-event?]
+   (try
+     ((get-db-fn-by-type (:type event)) (dissoc event :type))
+     (log/info "Fail write successfully written to DB: " event)
+     (-> events rest vec d/recur)
+     (catch Exception e
+       (log/error e "Retry write failed.")
+       (if new-event?
+         (d/recur (conj events (update event :retry inc)))
+         (d/recur (conj (vec (rest events)) (update event :retry inc))))))))
 
-    (fn [event]
-      (cond
-        (= event ::timeout)
-        (if-let [event (first events)]
-          (try
-            (if (>= (:retry event) 3)
+
+(defn- process-failed-db-writes []
+  (d/loop [events []]
+    (d/chain
+      (d/timeout!
+        (s/take! failed-write-stream ::drained)
+        1000
+        ::timeout)
+      (fn [data]
+        (if-not (#{::drained ::timeout} data)
+          (try-write-to-db data events)
+          (if-let [f-event (first events)]
+            (if (>= (:retry f-event) 3)
               (do
-                (log/info "Event: " event " is going to be discarded due to exceeding number of tries.")
-                (d/recur (vec (rest events))))
-              (do
-                ((get-db-fn-by-type (:type event)) (dissoc event :type))
-                (d/recur (vec (rest events)))))
-            (catch Exception e
-              (log/error e "Could not write event to DB." event)
-              (d/recur (vec (rest (conj events (update event :retry inc)))))))
-          (d/recur []))
-
-        (not= event ::drained)
-        (try
-          ((get-db-fn-by-type (:type event)) (dissoc event :type))
-          (log/info "Failed db write successfully written to DB.")
-          (d/recur events)
-          (catch Exception e
-            (log/error e "Could not write event to DB.")
-            (d/recur (conj events (update event :retry inc)))))
-
-        :else (d/recur events)))))
+                (log/warn "Discarding event due to max number of retry count." f-event)
+                (-> events rest vec d/recur))
+              (try-write-to-db f-event events false))
+            (d/recur [])))))))
 
 
-(defn process-db-stream []
-  (mc/async-loop 1 [events []]
-    (s/try-take! db-stream ::drained 20 ::timeout)
-
-    (fn [event]
-      (let [events      (if (#{::timeout ::drained} event) events (conj events event))
-            bulk-events (take 32 events)]
-        (if (write-to-db? event bulk-events)
-          (try
-            (log/debug "Writing events: " bulk-events)
-            (db/query :create-events! {:events bulk-events})
-            (->> events (drop (count bulk-events)) vec d/recur)
-            (catch Exception e
-              (log/error e "Could not write events to DB." bulk-events)
-              (doseq [[id write-key payload arrived-at] bulk-events]
-                (s/put! failed-write-stream {:id               id
-                                             :write_key  write-key
-                                             :arrived_at arrived-at
-                                             :payload    payload
-                                             :type       :event}))
-              (-> (drop (count bulk-events) events) vec d/recur)))
-          (d/recur events))))))
+(defn- process-db-stream []
+  (s/consume-async (fn [events]
+                     (try
+                       (log/info "Writing events to DB" events)
+                       (db/query :create-events! {:events events})
+                       (catch Exception e
+                         (log/error e "Failed to write events to DB!")
+                         (s/put-all! failed-write-stream (map
+                                                           (fn [[id write-key payload arrived-at]]
+                                                             {:id         id
+                                                              :write_key  write-key
+                                                              :payload    payload
+                                                              :arrived_at arrived-at
+                                                              :type       :raw})
+                                                           events)))))
+                   ;;TODO make configurable
+    (s/batch 32 100 db-stream)))
 
 
 (comment
-  (db/query :create-event! {:id (UUID/randomUUID)
-                            :arrived_at (Date.)
-                            :payload {:data 23}
-                            :write_key "12312asd2a"})
   (dotimes [_ 2]
     (put! {:id        (UUID/randomUUID)
            :write_key (nano-id 32)
@@ -117,7 +97,7 @@
   (process-failed-db-writes))
 
 
-(defn process-dest-stream []
+(defn- process-dest-stream []
   (mc/async-loop conf/cores []
     (s/take! stream ::drained)
 
@@ -130,8 +110,8 @@
 
     (fn [event]
       (println "New Hey: " event)
-      (http/post "https://www.google-analytics.com/collect"
-        {:headers h :form-params {:ev "ses"}}))
+      event
+      #_(http/post "https://www.google-analytics.com/collect" {:headers h :form-params {:ev "ses"}}))
 
                  ;; wait for the result from `f` to be realized, and
                  ;; recur, unless the stream is already drained
