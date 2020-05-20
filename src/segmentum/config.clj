5cc3f046a807c3edc90f6c386bbcd3944cea2ad7
ertucetin <ertu.ctn@gmail.com>
2020-05-20 16:06:56 +0300

    Adding get conf fn

--- src/segmentum/config.clj
+++ src/segmentum/config.clj
@@ -16,3 +16,26 @@
     [(args)
      (source/from-system-props)
      (source/from-env)]))
+
+
+(defn- type->cast-fn [type]
+  (case type
+    :str  str
+    :bool #(Boolean/parseBoolean %)
+    :int  #(Integer/parseInt %)
+    :long #(Long/parseLong %)
+    identity))
+
+
+(defn get-conf
+  ([key]
+   (get-conf key nil))
+  ([key type]
+   (get-conf key type nil))
+  ([key type default]
+   (or ((type->cast-fn type) (key env))
+       default)))
+
+(comment
+  (get-conf :java-home)
+  )
