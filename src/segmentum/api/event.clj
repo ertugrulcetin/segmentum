17ae147bee0bfb3919c37ab56609182c5b1d6a3a
ertucetin <ertu.ctn@gmail.com>
2020-05-21 15:33:12 +0300

    Wrapping up destination stream

--- src/segmentum/api/event.clj
+++ src/segmentum/api/event.clj
@@ -1,8 +1,9 @@
 (ns segmentum.api.event
   (:require [segmentum.util.imports :refer [resource]]
             [segmentum.util.macros :as mc]
-            [segmentum.config :as conf]
+            [segmentum.config :as conf :refer [env]]
             [segmentum.db.core :as db]
+            [segmentum.transformations.google-analytics :as trans.ga]
             [manifold.stream :as s]
             [manifold.deferred :as d]
             [throttler.core :refer [throttle-fn]]
@@ -18,16 +19,19 @@
 (defonce stream (s/stream 1024 xf))
 (defonce put! (throttle-fn (partial s/put! stream) 1000 :second))
 
+
 (def db-xf (map #(vector (:id %) (:write_key %) (:payload %) (:arrived_at %))))
 (defonce db-stream (s/stream 1024 db-xf))
 (defonce dest-stream (s/stream 1024))
 
+
 (def fail-xf (map #(assoc % :retry 0)))
 (defonce failed-write-stream (s/stream 512 fail-xf))
 
+
 (s/connect stream db-stream)
 ;;TODO backpressure'a neden oluyor
-;(s/connect stream dest-stream)
+(s/connect stream dest-stream)
 
 
 (defn- get-db-fn-by-type [type]
@@ -62,7 +66,7 @@
         (if-let [f-event (first events)]
           (if (>= (:retry f-event) 3)
             (do
-              (log/warn "Discarding event due to max number of retry count." f-event)
+              (log/warn "Discarding event due to max number of retry attempt." f-event)
               (-> events rest vec d/recur))
             (try-write-to-db f-event events false))
           (d/recur []))))))
@@ -87,37 +91,43 @@
     (s/batch 32 100 db-stream)))
 
 
+(defn- process-dest-stream []
+  (let [parallelism (if (:prod env) conf/cores 1)]
+    (mc/async-loop parallelism []
+      (s/take! dest-stream ::drained)
+
+      (fn [event]
+        (println " - Event: " event)
+        (if (identical? ::drained event)
+          ::drained
+          (identity event)))
+
+      (fn [event]
+        (log/info "New event: " event)
+        (d/timeout!
+          (d/chain'
+            (http/post "https://www.google-analytics.com/collect"
+              {:form-params (trans.ga/handler event)})
+            #(assoc % :event event))
+          1000
+          {::timeout true :event event}))
+
+      (fn [result]
+        (log/info "Result: " result)
+        (when-not (identical? ::drained result)
+          (let [{:keys [status body request-time event]} result]
+            (log/info "Status: " status " - Body: " body))
+          (d/recur))))))
+
+
 (comment
-  (dotimes [_ 5000]
+  (dotimes [_ 1]
     (put! {:id        (UUID/randomUUID)
            :write_key (nano-id 32)
            :payload   {:data (rand-int 1000)}}))
   (process-db-stream)
-  (process-failed-db-writes))
-
-
-(defn- process-dest-stream []
-  (mc/async-loop conf/cores []
-    (s/take! stream ::drained)
-
-                 ;; if we got a message, run it through `f`
-    (fn [event]
-      (println " - Event: " event)
-      (if (identical? ::drained event)
-        ::drained
-        (identity event)))
-
-    (fn [event]
-      (println "New Hey: " event)
-      event
-      #_(http/post "https://www.google-analytics.com/collect" {:headers h :form-params {:ev "ses"}}))
-
-                 ;; wait for the result from `f` to be realized, and
-                 ;; recur, unless the stream is already drained
-    (fn [result]
-      (println "Result: " (select-keys result [:status :body]))
-      (when-not (identical? ::drained result)
-        (d/recur)))))
+  (process-failed-db-writes)
+  (process-dest-stream))
 
 
 (resource event-processing
